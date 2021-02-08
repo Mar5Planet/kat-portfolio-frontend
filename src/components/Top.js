1eeb55a9d84a382cdd38bdc8b63ff3fd8ffee380
Mar5Planet <amustaf@email.arizona.edu>
2021-02-08 15:00:40 -0500

    style modification

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Top() {
     return (
-        <div className="top-section">
-            <div className="ny-background"></div>
-            <h1> KATARINA MILES </h1>
-            <h4>Artist and Experience Designer</h4>
+        <div className="ny-background">
+            <div className="top-section">
+                <h1> KATARINA MILES </h1>
+                <h4>Artist and Experience Designer</h4>
+            </div>
         </div>
     )
 }
